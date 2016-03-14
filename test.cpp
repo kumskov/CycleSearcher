aed8cf9fe9474e5febc1b3196fa52783804f5bd0
Mikhail Kumskov <mishaelification@gmail.com>
2016-03-14 14:05:18 +0300

    Tests for import/export

--- test.cpp
+++ test.cpp
@@ -32,22 +32,40 @@ int main()
 
 	Graph test;
 
-	test.buildGraph(c);
+	//test.buildGraph(c);
 
 	result = std::time(nullptr);
 	std::cout << std::asctime(std::localtime(&result)) << std::endl;
 
-/*
+
 	while(1)
 	{
 		std::cout << "Total amount of packages: " << size << std::endl;
 		std::cout << "Print number of package to get info: ";
 		int index;
 		std::cin >> index;
-		std::cout << std::endl << test.printInfo(index) << std::endl;
-		//std::cout << std::endl << c[index].getFullInfo() << std::endl;
+		//Package exportpkg = c[index];
+		//Package exportpkg2 = c[index+1];
+		//Package importpkg;
+		//Package importpkg2;
+		//std::string flname = "pkgtest";
+		//flname += std::to_string(index);
+		//std::ofstream _fli;
+		//std::ifstream _flo;
+		//_fli.open(flname.c_str(), std::ofstream::out);
+		//_fli << exportpkg;
+		//_fli << exportpkg2;
+		//_fli.close();
+		//std::cout << "Source pkg" << std::endl << exportpkg2.getFullInfo() << std::endl;
+		//_flo.open(flname.c_str(), std::ifstream::in);
+		//_flo >> importpkg;
+		//_flo >> importpkg2;
+		//_flo.close();
+		//std::cout << "Import pkg" << std::endl << importpkg2.getFullInfo() << std::endl;
+		//std::cout << std::endl << test.printInfo(index) << std::endl;
+		std::cout << std::endl << c[index].getFullInfo() << std::endl;
 	}
-	*/
+
 
 	return 0;
 }
