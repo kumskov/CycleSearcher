f2ef291e2b4a6c19a8e5b2ca1d9b92038b1a0afb
Mikhail Kumskov <mishaelification@gmail.com>
2016-05-03 23:39:34 +0300

    Added abstract class for all exporters

---
+++ exporter.hpp
@@ -0,0 +1,30 @@
+#ifndef __EXPORTER_ABSTRACT_HPP__
+#define __EXPORTER_ABSTRACT_HPP__
+
+#include <string>
+#include "classinfo.hpp"
+#include "cyclecontainer.hpp"
+#include "graph.hpp"
+
+class Exporter : public ClassInfo
+{
+public:
+	virtual void setFile(std::string) = 0;
+
+	virtual void setSlotIgnore(bool) = 0;
+	virtual bool getSlotIgnore() = 0;
+
+	virtual void setMarkBroken(bool) = 0;
+	virtual bool getMarkBroken() = 0;
+
+	virtual void setName(std::string) = 0;
+	virtual std::string getName() = 0;
+
+	virtual void generateFromGraph(Graph) = 0;
+	virtual void markCycles(CycleContainer) = 0;
+
+	virtual void save() = 0;
+
+};
+
+#endif
