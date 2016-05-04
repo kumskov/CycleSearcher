20ed7fa5a18b4667bc34957834dbc858fd7b597b
Mikhail Kumskov <mishaelification@gmail.com>
2016-05-04 21:25:23 +0300

    Small method for a status bar

---
+++ utils.cpp
@@ -0,0 +1,22 @@
+#include "utils.hpp"
+#include <iostream>
+
+std::string Utils::processString(int current, int max, int width)
+{
+	double onepercent = max/100.0;
+	double percentage = current/onepercent;
+
+	double currentprocess = (width/100.0)*percentage;
+
+	//std::cout << currentprocess << std::endl << std::endl;
+
+	std::string ret = "[";
+	for (int i = 0; i < width; ++i)
+	{
+		char toadd = (currentprocess >= i) ? '*' : ' ';
+		ret.push_back(toadd);
+	}
+	ret.push_back(']');
+
+	return ret;
+}
---
+++ utils.hpp
@@ -0,0 +1,13 @@
+#ifndef __UTILS_COURSEWORK_HPP__
+#define __UTILS_COURSEWORK_HPP__
+
+#include <string>
+
+class Utils
+{
+public:
+	static std::string processString(int, int, int);
+};
+
+
+#endif
